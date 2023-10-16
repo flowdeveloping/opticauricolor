36b8bc3a34ed90705185aa7f749ba6cfd86d05af
Jimena Nieto <82246454+flowdeveloping@users.noreply.github.com>
2023-10-16 16:40:01 +0200

    nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,52 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../assets/logoopticauricolor.png";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import "../styles/NavBar.css";
 
-const navbar = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+const Navbar = () => {
   const [openLinks, setOpenLinks] = useState(false);
 
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
 
-  
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setOpenLinks(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="navbar">
       <div className="ladoizquierdomenu" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
-        <div className="hiddenLinks">
-          <Link to="/">Inicio</Link>
-          <Link to="/nosotros">Nosotros</Link>
-          <Link to="/productos">Productos</Link>
-          <Link to="/">Promociones</Link>
-          <Link to="/">Salud Visual</Link>
-          <Link to="/">Contacto</Link>
-        </div>
+        {openLinks ? (
+          <div className="hiddenLinks">
+            <Link to="/">Inicio</Link>
+            <Link to="/nosotros">Nosotros</Link>
+            <Link to="/productos">Productos</Link>
+            <Link to="#">Promociones</Link>
+            <Link to="#">Salud Visual</Link>
+            <Link to="#">Contacto</Link>
+          </div>
+        ) : (
+          <img src={Logo} />
+        )}
       </div>
-      <div className=" ladomediomenu">
+      <div className="ladomediomenu">
         <Link to="/">Inicio</Link>
         <Link to="/nosotros">Nosotros</Link>
         <Link to="/productos">Productos</Link>
-        <Link to="/">Promociones</Link>
-        <Link to="/">Salud Visual</Link>
-        <Link to="/">Contacto</Link>
+        <Link to="#">Promociones</Link>
+        <Link to="#">Salud Visual</Link>
+        <Link to="#">Contacto</Link>
 
         <button onClick={toggleNavbar}>
           <MenuIcon />
@@ -44,4 +56,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
